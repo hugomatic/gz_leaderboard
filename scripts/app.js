81e37ff0c90e35102f38680801d8f3d6f418770f
Hugo Boyer <hugo@osrfoundation.org>
2015-04-02 13:57:00 -0700

    added missing files

--- scripts/app.js
+++ scripts/app.js
@@ -4,11 +4,24 @@
   document.addEventListener('polymer-ready', function() {
     // Perform some behaviour
     console.log('polymer-ready');
-    var tmpl = document.querySelector("#tmpl");
-    tmpl.title = "Simulation leaderboard";
-    tmpl.selected = "leaders";
-  });
+    var tmpl = document.querySelector("#tmpl")
+    tmpl.title = "Mentor 2 leaderboard"
+    tmpl.selected = "leaders"
+  })
 
+  setInterval(function () {
+     let leaderboard = document.querySelector("#leaderboard")
+     try {
+      leaderboard.refresh()  
+     }
+     catch(e)
+     {
+       console.log('error refresh ', e)
+     }
+     
+  }, 5000)
+
+  
 // wrap document so it plays nice with other libraries
 // http://www.polymer-project.org/platform/shadow-dom.html#wrappers
-})(wrap(document));
+})(wrap(document))
